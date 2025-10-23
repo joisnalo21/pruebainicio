0a58bff58f9554f86401d7e4b1a9e36a37425a01
Josué Navia <jinavia@utpl.edu.ec>
2025-10-22 23:07:27 -0500

    fix  appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        config(['app.start_time' => now()]);
+        Config::set('app.start_time', microtime(true));
 
     }
 }
