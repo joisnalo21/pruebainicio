27fd433190aca2634324b2eec7d5212b4fd91107
Josué Navia <jinavia@utpl.edu.ec>
2025-10-22 22:54:37 -0500

    routes para prometheus

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        config(['app.start_time' => now()]);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+oute::get('/metrics', function () {
+    $uptime = now()->diffInSeconds(config('app.start_time', now()));
+    $users = DB::table('users')->count();
+
+    $metrics = <<<EOT
+# HELP app_uptime_seconds Uptime of the Laravel app in seconds
+# TYPE app_uptime_seconds gauge
+app_uptime_seconds {$uptime}
+
+# HELP app_users_total Total number of users
+# TYPE app_users_total gauge
+app_users_total {$users}
+
+EOT;
+
+    return response($metrics, 200)->header('Content-Type', 'text/plain');
+});
+
+
